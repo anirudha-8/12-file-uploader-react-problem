641cf52739d65355d03032b075593424087ca111
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-16 23:46:38 +0530

    solve problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+	const [file, setFile] = useState(null);
 
-  return (
-    
-  )
+	const handleInputFile = (e) => {
+		setFile(e.target.files[0]);
+	};
+
+	return (
+		<div>
+			<input
+				type="file"
+				accept="images/*"
+				name="file"
+				id="file"
+				onChange={handleInputFile}
+			/>
+			<div>
+				{file && (
+					<img
+						src={URL.createObjectURL(file)}
+						alt="selected file preview"
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
